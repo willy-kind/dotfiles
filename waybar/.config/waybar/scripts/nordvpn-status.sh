bfa6ece672e6326b8999a4ed6661d2827fd8c140
Willy Kind <willy.kind@protonmail.com>
2025-05-11 14:11:32 +0200

    update waybar

--- waybar/.config/waybar/scripts/nordvpn-status.sh
+++ waybar/.config/waybar/scripts/nordvpn-status.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-STATUS=$(nordvpn status | grep Status | awk '{print $2}')
+get_country_code() {
+    case "$1" in
+        Germany) echo "DEU" ;;
+        United\ States) echo "USA" ;;
+        United\ Kingdom) echo "GBR" ;;
+        France) echo "FRA" ;;
+        Canada) echo "CAN" ;;
+        Netherlands) echo "NLD" ;;
+        Sweden) echo "SWE" ;;
+        Norway) echo "NOR" ;;
+        Denmark) echo "DNK" ;;
+        Finland) echo "FIN" ;;
+        Iceland) echo "ISL" ;;
+        Switzerland) echo "CHE" ;;
+        Australia) echo "AUS" ;;
+        Singapore) echo "SGP" ;;
+        Japan) echo "JPN" ;;
+        *) echo "UNK" ;;  # Unknown
+    esac
+}
+
+STATUS=$(nordvpn status | grep "Status" | awk '{print $2}')
 
 if [ "$STATUS" = "Connected" ]; then
-    echo '{"text":"󱐡 VPN","class":"connected"}'
+    COUNTRY=$(nordvpn status | grep "Country" | cut -d ':' -f2 | xargs)
+    CODE=$(get_country_code "$COUNTRY")
+    echo "{\"text\":\"$CODE 󱐡\",\"class\":\"connected\"}"
 else
-    echo '{"text":"󱐢 No VPN","class":"disconnected"}'
+    echo "{\"text\":\"󱐢\",\"class\":\"disconnected\"}"
 fi
 
