45c80bbb4d8d93dd097bc68fb92007f4505bd207
Willy Kind <willy.kind@protonmail.com>
2025-06-09 21:05:15 +0200

    init general devshell and add k9s config

---
+++ nix/devshells/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Declarative user environment via nix profile";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      # Optional dev shell
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [ neovim git ];
+        shellHook = ''echo "Welcome to dev shell"'';
+      };
+    }
+  );
+}
